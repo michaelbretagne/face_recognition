1a79aad50fe691843c87936d88117b01da94d84d
Michael <michael.bretagne@gmail.com>
2018-07-18 17:34:35 -0700

    Add ranking badge feature using AWS lambda function

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className="white f3">
-        {`${name}, your current entry count is...`}
+class Rank extends Component {
+  state = { emoji: "" };
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.entries === this.props.entries &&
+      prevProps.name === this.props.entries
+    ) {
+      return null;
+    }
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = entries => {
+    fetch(
+      `https://i1e1pzp9p2.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`,
+    )
+      .then(response => response.json())
+      .then(data => this.setState({ emoji: data.input }))
+      .catch(console.log);
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="white f3">
+          {`${this.props.name}, your current entry count is...`}
+        </div>
+        <div className="white f1">{this.props.entries}</div>
+        <div className="white f3">{`Rank Badge: ${this.state.emoji}`}</div>
       </div>
-      <div className="white f1">{entries}</div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Rank;
